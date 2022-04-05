3f56e04ffef8092aea57f1c480299cca4aa89990
Luis Borjas Reyes <luisfborjas@gmail.com>
2022-04-05 18:54:51 -0400

    wip: fleshing out the lil app

--- src/LunchLine/App.hs
+++ src/LunchLine/App.hs
@@ -6,11 +6,13 @@ module LunchLine.App where
 import Data.Pool
 import Database.Persist.Sqlite hiding (SqlPersistT)
 import Control.Monad.Reader
+    ( MonadIO(..), ReaderT(..), MonadReader, asks )
 import Control.Monad.Logger
 import LunchLine.Models
 import Data.List (foldl')
 import Data.Maybe
 import Database.Esqueleto.Experimental
+import Data.Time.Clock (getCurrentTime, UTCTime (UTCTime))
 
 data Env = Env
   { envPool :: Pool SqlBackend
@@ -40,32 +42,22 @@ runDB body = do
 -- As for RunDB, we're in the YesodDB instance, and run in a Handler context:
 -- https://github.com/MercuryTechnologies/mercury-web-backend/blob/70aa056ab6ce6d2d7cbc03917f2983a7b5896134/src/App.hs#L194-L208
 
-weeklyBudget :: Double
-weeklyBudget = 100
-
-spend :: Double -> LineItem -> Double
-spend n e = n - lineItemAmount e
-
-
-getLineItemTotal
-  :: (Num a, MonadIO m, PersistField a)
-  => SqlPersistT m a
-getLineItemTotal =
-  selectSum $ do
-    items <- from $ table @LineItem
-    pure $ sum_ $ items ^. LineItemAmount
-  where
-    selectSum = fmap (maybe 0 (fromMaybe 0 . unValue)) . selectOne
-
 runApp :: AppT ()
-runApp = do
-  total <- runDB $ do
-    insert_ $ LineItem "Pizza" 11.0
-    insert_ $ LineItem "Burger" 12.0
-    getLineItemTotal
-  let remainingBudget = weeklyBudget - total
-  liftIO . putStrLn $ "Remaining Budget:" <> show remainingBudget
+runApp = summary
 
+summary :: AppT ()
+summary = do
+  now@(UTCTime today _)<- liftIO getCurrentTime
+  remainingBudget <- runDB $ do
+    settings <- selectOne getSettings
+    case settings of
+      Nothing -> pure Nothing
+      Just (Entity _ Settings{settingsWeeklyBudget}) -> do
+        total <- getLineItemTotal (today, today)
+        pure . Just $ settingsWeeklyBudget - total
+  liftIO $ case remainingBudget of
+    Nothing -> putStrLn "No budget set"
+    Just rb -> putStrLn $ "Remaining Budget " <> show rb
 
 appMain :: IO ()
 appMain = do
@@ -74,6 +66,9 @@ appMain = do
     -- NOTE: the persistent book uses `runResourceT` here
     -- NOTE: from the exercise "figure out a way to move migrateAll to main"
     -- from: https://www.yesodweb.com/book/persistent#persistent_integration_with_yesod
-    runSqlPool (runMigration migrateAll) pool
+    --runSqlPool (runMigration migrateAll) pool
+    flip runSqlPool pool $ do
+      runMigration migrateAll
+      bootstrapInitialSettings
     let env = Env pool
     runAppT runApp env
--- src/LunchLine/Models.hs
+++ src/LunchLine/Models.hs
@@ -1,16 +1,86 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TypeApplications #-}
 -- NOTE: these extensions are now required by persistent,
 {-# LANGUAGE TypeFamilies, GADTs, StandaloneDeriving, UndecidableInstances, FlexibleInstances, MultiParamTypeClasses#-}
+{-# LANGUAGE OverloadedLabels #-}
 -- |
 
 module LunchLine.Models where
 
 import Database.Persist.TH
+import Database.Esqueleto.Experimental
+import Control.Monad.IO.Class
+import Data.Maybe
+import Data.Time.Calendar
+import Data.Time.Clock
 
 share [mkPersist sqlSettings, mkMigrate "migrateAll"][persistLowerCase|
 LineItem
   name String
   amount Double
+  added Day
+  created_at UTCTime
+  updated_at UTCTime
   deriving Show
+
+Settings
+  weeklyBudget Double default=100
+  weekStartsSunday Bool default=false
 |]
+
+data AddLineItem = AddLineItem
+  { addName :: String
+  , addAmount :: Double
+  }
+
+-- | Initialize settings unless already initialized
+bootstrapInitialSettings
+  :: (MonadIO m)
+  => SqlPersistT m ()
+bootstrapInitialSettings = do
+  existingSettings <- selectOne $ do
+    settingsSingleton <- from $ table @Settings
+    limit 1
+    pure settingsSingleton
+  case existingSettings of
+    Nothing -> insert_ $ Settings 100 False
+    Just _es -> pure ()
+
+
+getSettings :: SqlQuery (SqlExpr (Entity Settings))
+getSettings = do
+  s <- from $ table @Settings
+  limit 1
+  pure s
+
+getItemsInInterval :: (Day, Day) -> SqlQuery (SqlExpr (Entity LineItem))
+getItemsInInterval (iStart, iEnd) = do
+  items <- from $ table @LineItem
+  where_ $ items ^. #added `between` (val iStart, val iEnd)
+  pure items
+
+
+getLineItemTotal
+  :: (Num a, MonadIO m, PersistField a)
+  => (Day, Day) -> SqlPersistT m a
+getLineItemTotal interval =
+  selectSum $ do
+    items <- getItemsInInterval interval
+    pure $ sum_ $ items ^. LineItemAmount
+  where
+    selectSum = fmap (maybe 0 (fromMaybe 0 . unValue)) . selectOne
+
+addItem
+  :: MonadIO m
+  => AddLineItem -> SqlPersistT m ()
+addItem AddLineItem{addName, addAmount} = do
+  now@(UTCTime today _) <- liftIO getCurrentTime
+  let li =
+        LineItem
+          addName
+          addAmount
+          today
+          now
+          now
+  insert_ li
